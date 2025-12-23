43ca83c66aa26f2b181c44f20b71055be4818d62
dev491999 <devashishgpatil@gmail.com>
2025-12-23 14:42:26 +0530

    footer check

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,11 @@
-fetch("/assets/partials/footer.html")
-  .then(response => response.text())
+fetch("assets/partials/footer.html")
+  .then(response => {
+    if (!response.ok) throw new Error("Footer not found");
+    return response.text();
+  })
   .then(html => {
     document.getElementById("site-footer").innerHTML = html;
+  })
+  .catch(err => {
+    console.error(err);
   });
